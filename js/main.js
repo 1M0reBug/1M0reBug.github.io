196e2fc311a2c01b7f4a243ca7d6258811f06249
1M0reBug <jordan.quagliatini@gmail.com>
2017-01-10 23:09:47 +0100

    Removing angular (using gnal.js)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-(function () {
 /* eslint-disable */
-var app = angular.module('jordanModule', ['pascalprecht.translate', 'ngSanitize']);
-
-app.config(function($translateProvider) {
-  $translateProvider.useSanitizeValueStrategy('sanitize');
-  $translateProvider.translations('en', {
+;(function () {
+  var _ = new Gnal({ useHash : true });
+  _.translations('en', {
       TITLE: 'Jordan Quagliatini - Young developer interested in every kind of new technology',
-      hello: "Hi, I'm <span class='hand'>Jordan</span>",
+      hello: "#!> Hi, I'm <span class='hand'>Jordan</span>",
       "sub-hello": 'Young developer interested in every kind of new technology',
       introductionTitle: "Let me introduce me",
       introduction: "I'm a future software engineer, partially studying at CPE Lyon (Fr). In the same time I work for Orange France, the operator.",
@@ -22,10 +19,10 @@ app.config(function($translateProvider) {
       degreeTitle: "Technology degree - University Institute of Technology Arles",
       degree: "Studying computer science, discovering development",
       degreeDate: "June 2014",
-      contact:"<span class='mdi-communication-email'></span> Contact me",
-      connect: "<span class='icon-linkedin'></span> Connect with me",
-      follow: "<span class='icon-github'></span> Follow me",
-      pdfResume: "You can also <a href='/cv'>look</a> my resume",
+      contact:"#!> <span class='mdi-communication-email'></span> Contact me",
+      connect: "#!> <span class='icon-linkedin'></span> Connect with me",
+      follow: "#!> <span class='icon-github'></span> Follow me",
+      pdfResume: "#!> You can also <a href='/cv'>look</a> my resume",
       connectFooter: "Connect",
       agility: "Agility",
       javaContent: "I started Java during my technical degree, and continued. It's kind of THE language to know today. Not a lot of experience with it, but kind of enthusiast to use it.",
@@ -37,18 +34,17 @@ app.config(function($translateProvider) {
       gitContent: "I discovered Git during an internship, it was just like a revelation for me. For me you can't be a developer without knowing this fantastic tool.",
       agilityContent: "I'm one of these young and inexperienced student who thinks agility is still a way of life, maybe because I've never experienced a Real Agile project, I hope to try it one day.",
       readMoreSkills: "Read More"
-  });
-
-  $translateProvider.translations('fr', {
+  })
+  .translations('fr', {
       TITLE: 'Jordan Quagliatini - Jeune développeur passionné par toutes sortes de nouvelles technologies',
-      hello: "Salut, je suis <span class='hand'>Jordan</span>",
+      hello: "#!> Salut, je suis <span class='hand'>Jordan</span>",
       "sub-hello": 'Jeune développeur passionné par toutes sortes de nouvelles technologies',
       introductionTitle: "Je me présente",
       introduction: "Futur ingénieur logiciel, alternant à CPE Lyon et développeur chez Orange France.",
       skillTitle: "Compétences",
       educationTitle: "Formation",
-      bachelorCPETitle: "1<sup>ère</sup> année, diplôme d'ingénieur",
-      bachelorCPE: "1<sup>ère</sup> année de mon diplôme d'ingénieur chez CPE Lyon.",
+      bachelorCPETitle: "#!> 1<sup>ère</sup> année, diplôme d'ingénieur",
+      bachelorCPE: "#!> 1<sup>ère</sup> année de mon diplôme d'ingénieur chez CPE Lyon.",
       timelineMoreInfosBtn: "En savoir plus",
       bachelorCPEDate: "Septembre 2015 - ?",
       bachelorLPTitle: "Licence professionnelle SIL - DASI",
@@ -57,10 +53,10 @@ app.config(function($translateProvider) {
       degreeTitle: "DUT Informatique",
       degree: "Découverte de la programmation",
       degreeDate: "Juin 2014",
-      contact: "<span class='mdi-communication-email'></span> Me contacter",
-      connect: "<span class='icon-linkedin'></span> Se connecter",
-      follow: "<span class='icon-github'></span> Me suivre",
-      pdfResume: "Vous pouvez aussi <a href='/cv' target='_blank'>visualiser</a> mon CV",
+      contact: "#!> <span class='mdi-communication-email'></span> Me contacter",
+      connect: "#!> <span class='icon-linkedin'></span> Se connecter",
+      follow: "#!> <span class='icon-github'></span> Me suivre",
+      pdfResume: "#!> Vous pouvez aussi <a href='/cv' target='_blank'>visualiser</a> mon CV",
       connectFooter: "Se connecter",
       agility: "Agilité",
       javaContent: "J'ai commencé le Java pendant mon DUT. C'est LE language à connaitre aujourd'hui. Je n'ai pas beaucoup d'expérience avec, mais je serais très intéressé de travailler avec.",
@@ -72,11 +68,10 @@ app.config(function($translateProvider) {
       gitContent: "J'ai découvert Git pendant mon stage de DUT, ce qui a été comme une révélation pour moi. Selon moi on ne peut être un développeur sans connaitre cet outil.",
       agilityContent: "Je suis un de ces développeurs jeunes et inexpérimentés, qui pensent encore que l'agilité est un mode de vie. Peut-être parce que je n'ai jamais vécu de vrai projet Agile, ce que j'espère combler bientôt.",
       readMoreSkills: "Plus"
-  });
-
-  $translateProvider.translations('it', {
+  })
+  .translations('it', {
       TITLE: 'Jordan Quagliatini - Giovanne programatore, appassionato di tutti i tipi di nuove tecnologie',
-      hello: "Ciao, sono <span class='hand'>Jordan</span>",
+      hello: "#!> Ciao, sono <span class='hand'>Jordan</span>",
       "sub-hello" : 'Giovane programmatore, appassionato di tutti i tipi di nuove tecnologie',
       introductionTitle: "Mi presento",
       introduction: "Futuro ingegnere, attualmente apprendista, studio a CPE Lyon e lavoro per Orange France",
@@ -92,10 +87,10 @@ app.config(function($translateProvider) {
       degreeTitle: "Diploma Universitario",
       degree: "Durante questo primo diploma imparo la programmazione",
       degreeDate: "Giugno 2014",
-      contact: "<span class='mdi-communication-email'></span> Contattami",
-      connect: "<span class='icon-linkedin'></span> Connessione",
-      follow: "<span class='icon-github'></span> Seguimi",
-      pdfResume: "<a href='/cv' target='_blank'>Il mio CV</a>",
+      contact: "#!> <span class='mdi-communication-email'></span> Contattami",
+      connect: "#!> <span class='icon-linkedin'></span> Connessione",
+      follow: "#!> <span class='icon-github'></span> Seguimi",
+      pdfResume: "#!> <a href='/cv' target='_blank'>Il mio CV</a>",
       connectFooter: "Connessione",
       agility: "Agilità",
       javaContent: "Ho cominciato Java durante il mio diploma Universitario. Considerato IL LINGUAGGIO oggi. Non è il linguaggio con cui ho più esperienze, però mi piaccerebbe usarlo.",
@@ -107,26 +102,17 @@ app.config(function($translateProvider) {
       gitContent: "Ho scoperto Git durante una formazione di fine diploma. È stata una rivelazione. Secondo me non si può essere programatore senza conoscere questo strumento.",
       agilityContent: "Sono uno di questi programmatori, ancora giovane che pensa che l'agilità è un modo di vità, forse perchè non ho mai provato un vero progetto Agile. Non vedo l'ora di provarne uno.",
       readMoreSkills: "saperne di più"
-  });
+  })
+  .choose('en')
+  .translate();
 
-  $translateProvider.preferredLanguage('en');
-});
-/* eslint-enable */
-
-  app.controller('Ctrl', function ($scope, $translate, $rootScope, $location) {
-    $scope.changeLanguage = function (key) {
-      $translate.use(key);
-      $rootScope.title = $translate('TITLE');
-      $location.path(key);
-    };
-    var path = $location.path().replace('/', '');
-    var lang = $translate.use();
-    if (path.length !== 0) {
-      if (path !== lang && ['fr', 'en', 'it'].indexOf(path) !== -1) {
-        $scope.changeLanguage(path);
-      }
-    } else {
-      $location.path(lang);
-    }
-  });
+  var list = document.querySelectorAll('.btn-change-lang');
+  for (var i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      console.log('coucou');
+      var selectedLang = event.target.dataset.lang;
+      _.changeTo(selectedLang);
+    });
+  }
 })();
